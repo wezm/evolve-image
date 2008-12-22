4d2eb22941d9a86dadba0c56fe72adf22bcda565
Wesley Moore <Wesley.Moore@nab.com.au>
2008-12-22 13:28:01 +1100

    Ensure cairo was built with PNG functions

--- EICairoImage.h
+++ EICairoImage.h
@@ -2,6 +2,10 @@
 #import "EIImage.h"
 #import <cairo.h>
 
+#ifndef CAIRO_HAS_PNG_FUNCTIONS
+#error Cairo does not have required PNG support
+#endif
+
 @interface EICairoImage : EIImage {
 	cairo_surface_t *surface;
 }
